29532e04e67414a96e0b01ca26e5151e7ed890e2
Anh-vu <anhuvdinh@gmail.com>
2021-08-21 21:56:50 -0500

    commit tree e862a83e04a67126f943a0766d05ff05b4f12859

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+const refreshBtn = document.createElement("BUTTON")
+refreshBtn.textContent = 'Refresh Page'
+refreshBtn.addEventListener('click', doNothing)
+body.append(refreshBtn)
+
+function doNothing(){
+    return testVar;
+}
